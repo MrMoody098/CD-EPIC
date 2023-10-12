df0a21e82a3ab29c17e928250dea03462a92b37b
ConorBobbleHat <c.github@firstpartners.net>
2023-10-12 12:20:55 +0100

    add org json dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,8 @@
 module com.cd.quizwhiz {
+    requires transitive javafx.graphics;
     requires javafx.controls;
     requires javafx.web;
+    requires org.json;
+
     exports com.cd.quizwhiz;
 }
