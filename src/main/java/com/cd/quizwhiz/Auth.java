7657b3d9cdcf526269b4ea6072ce84e710fa6d6d
MrMoody098 <122175726+MrMoody098@users.noreply.github.com>
2023-10-16 19:26:40 +0100

    Auth class handles login and register of users both methods are Auth.login() and Auth.register

---
+++ src/main/java/com/cd/quizwhiz/Auth.java
@@ -0,0 +1,61 @@
+package com.cd.quizwhiz;
+
+import java.io.*;
+
+// Auth class handles user registration and login.
+public class Auth {
+    // Directory where user data files are stored.
+    private String userFolder = "users";
+
+    // The register method checks if a file with the username already exists,
+    // and if not, it creates a new file with the username and password.
+    public String register(String username, String password) {
+        // Construct the filename based on the username.
+        String userDataFileName = username + ".txt";
+        File userFile = new File(userFolder, userDataFileName);
+
+        // If there is no file in the "users" folder with the given username.
+        if (!userFile.exists()) {
+            // Try to write a new file with the user's data.
+            try {
+                FileWriter writer = new FileWriter(userFile);
+                // Write the user's password into the file.
+                writer.write(password);
+                writer.close();
+                return username; // Registration successful.
+            } catch (IOException e) {
+                return "Error creating user file."; // Handle file creation error.
+            }
+        } else {
+            return "Username already exists."; // Username is already taken.
+        }
+    }
+
+    // Login method verifies if the entered credentials match a previously registered user's credentials.
+    public boolean Login(String username, String password) {
+        boolean loggedIn = false; // Tracks whether the user is logged in or not.
+
+        // Construct the filename based on the username.
+        String userDataFileName = username + ".txt";
+        File userFile = new File(userFolder, userDataFileName);
+
+        try (BufferedReader br = new BufferedReader(new FileReader(userFile))) {
+            String line = br.readLine();
+            if (line != null) {
+                String storedPassword = line;
+                // Check if the provided password matches the stored password.
+                if (password.equals(storedPassword)) {
+                    loggedIn = true;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            // Handle the file not found exception.
+            loggedIn = false;
+        } catch (IOException e) {
+            // Handle other I/O exceptions.
+            loggedIn = false;
+        }
+
+        return loggedIn;
+    }
+}
