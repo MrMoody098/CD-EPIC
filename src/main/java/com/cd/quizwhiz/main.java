73bf3543b0dc52ca4a789101d109f6abf1df2dd6
MrMoody098 <122175726+MrMoody098@users.noreply.github.com>
2023-10-19 17:40:39 +0100

    few changes

---
+++ src/main/java/com/cd/quizwhiz/main.java
@@ -0,0 +1,15 @@
+package com.cd.quizwhiz;
+
+public class main {
+    public static void main(String[] args){
+        User user1 = new User("jondo");
+        user1.AddScore();
+        user1.AddScore();
+        user1.FinalScore();
+        user1.AddScore();
+        user1.FinalScore();
+        System.out.println(user1.GetMean());
+        System.out.println(user1.GetDeviation());
+        System.out.println(user1.GetMedian());
+    }
+}
