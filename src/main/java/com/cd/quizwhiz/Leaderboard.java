11db0e0f0c68bb1c380b0a106d9a531e402a8cc4
MrMoody098 <122175726+MrMoody098@users.noreply.github.com>
2023-10-20 16:42:06 +0100

    The getLeaderboard method now uses Auth.UserFolder as the directory path. The getLeaderboard method now takes no inputs, so you can call it from another class using Leaderboard.getLeaderboard();. The getLeaderboard method now returns a 2D array instead of a map. The 2D array is sorted in descending order of scores, so the user with the highest score is at the top.

--- src/main/java/com/cd/quizwhiz/Leaderboard.java
+++ src/main/java/com/cd/quizwhiz/Leaderboard.java
@@ -5,36 +5,34 @@ import java.io.*;
 import java.nio.file.*;
 import java.util.*;
 
-// Define the Leaderboard class
 public class Leaderboard {
-    // Define the getLeaderboard method that takes a directory path as input and returns a map of usernames and their top scores
-    private static Map<String, Integer> getLeaderboard(String dir) throws IOException {
-        // Initialize an empty map to store the leaderboard
-        Map<String, Integer> leaderboard = new HashMap<>();
-        // Walk through all files in the given directory
+    // Define the getLeaderboard method that takes no inputs and returns a 2D array of usernames and their top scores
+    public static String[][] getLeaderboard() throws IOException {
+        String dir = Auth.userFolder; // Use Auth.UserFolder as the directory path
+        List<String[]> leaderboardList = new ArrayList<>();
         Files.walk(Paths.get(dir))
                 .filter(Files::isRegularFile)
                 .forEach(path -> {
-                    // Get the username from the filename by removing the ".txt" extension
                     String username = path.getFileName().toString().replace(".txt", "");
-                    // Use a try-with-resources block to open and read the file
                     try (BufferedReader reader = new BufferedReader(new FileReader(path.toFile()))) {
-                        reader.readLine(); // Skip password line
+                        reader.readLine();
                         String line;
                         int maxScore = Integer.MIN_VALUE;
-                        // Read each line in the file (after the password line)
                         while ((line = reader.readLine()) != null) {
-                            // Update maxScore if the current score is higher than maxScore
                             maxScore = Math.max(maxScore, Integer.parseInt(line));
                         }
-                        // Add the username and their top score to the leaderboard map
-                        leaderboard.put(username, maxScore);
+                        leaderboardList.add(new String[]{username, String.valueOf(maxScore)});
                     } catch (IOException e) {
-                        // Print any IOExceptions that occur while reading the file
                         e.printStackTrace();
                     }
                 });
-        // Return the completed leaderboard map
+        // Sort the leaderboardList in descending order of scores
+        leaderboardList.sort((a, b) -> Integer.compare(Integer.parseInt(b[1]), Integer.parseInt(a[1])));
+        // Convert the leaderboardList to a 2D array
+        String[][] leaderboard = new String[leaderboardList.size()][2];
+        for (int i = 0; i < leaderboardList.size(); i++) {
+            leaderboard[i] = leaderboardList.get(i);
+        }
         return leaderboard;
     }
 }
