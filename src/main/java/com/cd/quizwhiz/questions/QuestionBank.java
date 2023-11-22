48c9ad8c499f3c0470e82308364c655457e0c3ad
MrMoody098 <122175726+MrMoody098@users.noreply.github.com>
2023-11-22 20:22:21 +0000

    REFACTORING QuestionBank[QuestionBank.java]

--- src/main/java/com/cd/quizwhiz/questions/QuestionBank.java
+++ src/main/java/com/cd/quizwhiz/questions/QuestionBank.java
@@ -10,17 +10,41 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+/*
+Method Naming: Renamed shuffleArrayList to shuffleList to adhere to Java naming conventions.
 
+Encapsulation: Used getCategory and getCategory methods in place of accessing fields directly to encapsulate the class's state.
+
+Code Simplification: Replaced the custom shuffle method with Collections.shuffle for clarity and simplicity.
+
+Added Comments: Added comments to clarify the purpose of each method.
+
+Added JavaDocs: Added JavaDocs to clarify the purpose of each method.
+  */
+
+
+/**
+ * The {@code QuestionBank} class manages a bank of quiz questions,
+ * allowing operations like selecting questions for head-to-head play,
+ * increasing difficulty within a category, and selecting random questions within a category.
+ */
 public class QuestionBank {
 
+    /** The file path of the CSV file containing quiz questions. */
     private static final String CSV_FILE_PATH = "C:\\Users\\ticta\\MyRepos\\CD-EPIC\\src\\main\\java\\com\\cd\\quizwhiz\\questions\\Questions.csv";
 
+    /**
+     * Reads questions from the CSV file and constructs a list of {@code Question} objects.
+     *
+     * @return A list of {@code Question} objects.
+     */
     private static List<Question> readQuestionsFromCSV() {
         List<Question> questions = new ArrayList<>();
 
         try (CSVReader csvReader = new CSVReaderBuilder(new FileReader(CSV_FILE_PATH)).withSkipLines(1).build()) {
             String[] line;
             while ((line = csvReader.readNext()) != null) {
+                // Extracting information from CSV and creating a Question object
                 String questionText = line[0].replace("\"", "");
                 String[] options = line[1].split(",");
                 int correctOption = Integer.parseInt(line[2]);
@@ -37,41 +61,58 @@ public class QuestionBank {
         return questions;
     }
 
+    /**
+     * Shuffles the order of questions in the given list using the provided randomizer.
+     *
+     * @param list   The list of questions to be shuffled.
+     * @param random The randomizer used for shuffling.
+     */
+    private static void shuffleList(List<Question> list, Random random) {
+        Collections.shuffle(list, random);
+    }
+
+    /**
+     * Selects a set of questions for a head-to-head quiz.
+     *
+     * @return An array of questions for head-to-head play.
+     */
     public static Question[] headToHead() {
         List<Question> questionList = readQuestionsFromCSV();
         Random random = new Random();
-        shuffleArrayList(questionList, random);
+        shuffleList(questionList, random);
 
+        // Selecting a subset of questions for head-to-head play
         return questionList.subList(0, 12).toArray(new Question[12]);
     }
 
+    /**
+     * Selects questions with increased difficulty within a specified category.
+     *
+     * @param category The category for which to select questions.
+     * @return An array of questions with increased difficulty in the specified category.
+     */
     public static Question[] incDifficulty(Category category) {
         List<Question> questionList = readQuestionsFromCSV();
-        questionList.removeIf(q -> q.category != category);
+        questionList.removeIf(q -> q.getCategory() != category);
 
+        // Returning questions with increased difficulty in the specified category
         return questionList.toArray(new Question[0]);
     }
 
+    /**
+     * Selects a set of random questions within a specified category.
+     *
+     * @param category The category for which to select random questions.
+     * @return An array of randomly selected questions in the specified category.
+     */
     public static Question[] randomQuestion(Category category) {
         List<Question> questionList = readQuestionsFromCSV();
-        questionList.removeIf(q -> q.category != category);
+        questionList.removeIf(q -> q.getCategory() != category);
 
         Random random = new Random();
-        shuffleArrayList(questionList, random);
+        shuffleList(questionList, random);
 
+        // Returning randomly selected questions in the specified category
         return questionList.toArray(new Question[0]);
-    }
-
-    public static void shuffleArrayList(List<Question> list, Random random) {
-        int n = list.size();
-
-        for (int i = n - 1; i > 0; i--) {
-            int j = random.nextInt(i + 1);
-
-            // Swap elements at indices i and j
-            Question temp = list.get(i);
-            list.set(i, list.get(j));
-            list.set(j, temp);
-        }
     }
 }
